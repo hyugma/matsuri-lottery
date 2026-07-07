d87d96958177cfabb2b175e6efa6c13f8e08f0f1
mhyugaji <mhyugaji>
2026-07-08 00:02:09 +0900

    Initial commit

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'matsuri-lottery-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css?v=5',
+  './app.js?v=2',
+  './manifest.json',
+  './icon-512.png',
+  './assets/1.mp3',
+  './assets/2.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
